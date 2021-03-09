b6d5c1e15319cda376ca3c046966782fd9f22e18
GermanFA <79627686+GermanFA@users.noreply.github.com>
2021-03-09 13:39:42 +0300

    Добавьте файлы проекта.

---
+++ Cycle_numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cycle_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numder = 7;
+            
+            
+
+            while (numder != 105)
+            {
+                Console.WriteLine(numder);
+                numder += 7;
+               
+            }
+        }
+    }
+}
